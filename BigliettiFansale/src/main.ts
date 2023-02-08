bb763f096b37605da3a23763b3612a2364f00dab
0xMere <merenda.s.mattia@gmail.com>
2023-02-08 12:31:49 +0100

    Commit generale

--- BigliettiFansale/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { Telegraf } from "telegraf";
-
-const bot = new Telegraf("5639268501:AAE7wTZjdPnrRsJOcedbV3IFYIFtCi-kdkw");
-
-bot.on("message", (ctx) => {
-  ctx.reply("Ciao, sono un bot molto stupido!");
-});
-
-bot.launch();
